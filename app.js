577c0eea31286bb202be458dd8c0411bce926cba
usr6902 <usr6902@gmail.com>
2025-01-16 01:53:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+location.href = "/turgo";
 String.prototype.replaceAt = function (index, replacement) {
   return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
