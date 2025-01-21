1ba9978ed315bbe03ce94766c3100ec4cd8b0d75
usr6902 <usr6902@gmail.com>
2025-01-21 03:49:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function getConfigValues() {
 }
 
 //location.href = "/turgo";
-location.href = getConfigValues().startupUrl ?? "/lingobasic/v2";
+location.href = getConfigValues().startupUrl || "/lingobasic/v2";
 String.prototype.replaceAt = function (index, replacement) {
   return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
